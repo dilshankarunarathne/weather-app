c9343fd0f647aeb4f4e7d744596ddac9d1fb2dc1
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-06-24 14:29:22 +0530

    Update App.jsx

--- WEATHER-APP/src/App.jsx
+++ WEATHER-APP/src/App.jsx
@@ -1,34 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import search from './assets/icons/search.svg'
+import { useStateContext } from './Context'
+import { BackgroundLayout, WeatherCard, MiniCard } from './Components'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [input, setInput] = useState('')
+  const { weather, thisLocation, values, place, setPlace } = useStateContext()
+  // console.log(weather)
+
+  const submitCity = () => {
+    setPlace(input)
+    setInput('')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-full h-screen text-white px-8'>
+      <nav className='w-full p-3 flex justify-between items-center'>
+        <h1 className='font-bold tracking-wide text-3xl'>Weather App</h1>
+        <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+          <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
+          <input onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              // sumit the form
+              submitCity()
+            }
+          }} type="text" placeholder='Search city' className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)} />
+        </div>
+      </nav>
+      <BackgroundLayout></BackgroundLayout>
+      <main className='w-full flex flex-wrap gap-8 py-4 px-[10%] items-center justify-center'>
+        <WeatherCard
+          place={thisLocation}
+          windspeed={weather.wspd}
+          humidity={weather.humidity}
+          temperature={weather.temp}
+          heatIndex={weather.heatindex}
+          iconString={weather.conditions}
+          conditions={weather.conditions}
+        />
+
+        <div className='flex justify-center gap-8 flex-wrap w-[60%]'>
+          {
+            values?.slice(1, 7).map(curr => {
+              return (
+                <MiniCard
+                  key={curr.datetime}
+                  time={curr.datetime}
+                  temp={curr.temp}
+                  iconString={curr.conditions}
+                />
+              )
+            })
+          }
+        </div>
+      </main>
+    </div>
   )
 }
 
