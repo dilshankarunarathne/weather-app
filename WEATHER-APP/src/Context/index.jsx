8a998ac4137ff84f258ff9de9f3592918411f4c4
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-06-24 20:03:04 +0530

    fixed: param name

--- WEATHER-APP/src/Context/index.jsx
+++ WEATHER-APP/src/Context/index.jsx
@@ -6,7 +6,7 @@ const StateContext = createContext()
 export const StateContextProvider = ({ children }) => {
     const [weather, setWeather] = useState({})
     const [values, setValues] = useState([])
-    const [place, setPlace] = useState('Jaipur')
+    const [place, setPlace] = useState('Colombo')
     const [thisLocation, setLocation] = useState('')
 
     const fetchWeather = async () => {
@@ -15,13 +15,13 @@ export const StateContextProvider = ({ children }) => {
             url: 'https://weather-api99.p.rapidapi.com/weather',
             params: {
                 aggregateHours: '24',
-                location: place,
+                city: place,
                 contentType: 'json',
                 unitGroup: 'metric',
                 shortColumnNames: 0,
             },
             headers: {
-                'X-RapidAPI-Key': c4bd239705mshf56569965352bddp1c42e5jsn305d5cf1a62b,
+                'X-RapidAPI-Key': 'c4bd239705mshf56569965352bddp1c42e5jsn305d5cf1a62b',
                 'X-RapidAPI-Host': 'weather-api99.p.rapidapi.com'
             }
         }
